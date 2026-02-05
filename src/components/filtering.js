ce2ec7b5525ddf27c4b2df00585a77c5be96c67c
Kristina Guschina <kristyshka2001@yandex.com>
2026-02-05 16:20:30 +0300

    Create table

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,14 +1,47 @@
-import {createComparison, defaultRules} from "../lib/compare.js";
+import { createComparison, defaultRules } from "../lib/compare.js";
 
-// @todo: #4.3 — настроить компаратор
+// #4.3 — настроить компаратор
+const compare = createComparison(defaultRules);
 
 export function initFiltering(elements, indexes) {
-    // @todo: #4.1 — заполнить выпадающие списки опциями
+  //— заполнить выпадающие списки опциями
+  Object.keys(indexes) // Получаем ключи из объекта
+    .forEach((elementName) => {
+      // Перебираем по именам
+      elements[elementName].append(
+        // в каждый элемент добавляем опции
+        ...Object.values(indexes[elementName]) // формируем массив имён, значений опций
+          .map((name) => {
+            // используйте name как значение и текстовое содержимое
+            //  создать и вернуть тег опции
+            const option = document.createElement("option");
 
-    return (data, state, action) => {
-        // @todo: #4.2 — обработать очистку поля
+            option.setAttribute("value", name);
+            option.textContent = name;
 
-        // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
-    }
+            return option;
+          }),
+      );
+    });
+
+  return (data, state, action) => {
+    // — обработать очистку поля
+    const ceDate = document.querySelector('input[name="date"]');
+    const ceCustomer = document.querySelector('input[name="customer"]');
+
+    if (action)
+      switch (action.name) {
+        case "clear":
+          if (action.getAttribute("data-field") == "date") {
+            ceDate.value = "";
+          } else {
+            ceCustomer.value = "";
+          }
+
+          break;
+      }
+
+    //  — отфильтровать данные используя компаратор
+    return data.filter((row) => compare(row, state));
+  };
 }
